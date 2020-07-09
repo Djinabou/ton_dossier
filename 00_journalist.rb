3cdf6cccd4299f83d12d0be38e70dedf2a90bcda
Djinabou <67662222+Djinabou@users.noreply.github.com>
2020-07-09 23:42:23 +0200

    Add files via upload

---
+++ 00_journalist.rb
@@ -0,0 +1,8 @@
+# combien y a-t-il de handle dans la liste
+puts handle number
+
+#Quelle est le handle le plus court de catte list
+puts [handle.shorter]
+
+#Quelle est la position dans l'array de la personne @epenser
+puts [position@epenser]
